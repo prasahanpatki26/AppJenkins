583559109620cca085d07cdb2c161ec58c9947ab
prasahanpatki26 <iamprajwal9@gmail.com>
2022-08-12 14:31:45 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MyCalc;
+
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Jetkins");
+            Calc calc = new MyCalc();
+            Console.WriteLine($"Sum = {calc.Add(70, 80)}");
+            Console.WriteLine($"Multiply = {calc.Add(70, 80)}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MyCalc/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyCalc
+{
+    public class Class1
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Multiply(int x,int y)
+        {
+            return x * y;
+        }
+    }
+}
